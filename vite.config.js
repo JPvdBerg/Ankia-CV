6ecfe291ec383e29f0b388dc16734e162aebaa1f
Jan-Paul van den Berg <janpaulvdberg@gmail.com>
2025-11-22 17:24:01 +0200

    Transform design to professional theme

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Use the repository name as the base path
-  base: '/Virtual-CV/', 
+  // This must match your repository name exactly
+  base: '/Ankia-CV/', 
   plugins: [react()],
 })
